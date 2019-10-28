3e9d4e6a7f15570ad1ba534d5f5be753a7beb8f3
Student <example@example.com>
2019-10-28 10:50:56 -0700

    create loops to dry out the code

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,24 @@
 $(document).ready(function() {
-  $("#formOne").submit(function() {
-    $(".person1").append("blah blah");
-    $(".person2").append("blah blah");
-    $(".animal").append("blah blah");
-    $(".exclamation").append("blah blah");
-    $(".verb").append("blah blah");
-    $(".noun").append("blah blah");
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+    var blanks = ["person1", "person2", "animal","exclamation", "verb",
+  "noun"];
+
+    blanks.forEach(function(blank) {
+      var userInput = $("#" + blank).val();
+      $("." + blank).text(userInput);
+
+    });
+
+
+
+    //
+    // $(".person1").append("blah blah");
+    // $(".person2").append("blah blah");
+    // $(".animal").append("blah blah");
+    // $(".exclamation").append("blah blah");
+    // $(".verb").append("blah blah");
+    // $(".noun").append("blah blah");
 
     $("#story").show();
   });
